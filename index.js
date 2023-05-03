cd715ff6414c1be2111caf562253db8d23307451
D9dman <tawhid268@gmail.com>
2023-05-04 01:12:26 +0600

    updated the api with dynamic data of each chef with find method

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.get("/", (req, res) => {
   res.json(allChefs);
 });
 
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const chef = allChefs?.find((chef) => chef.id == id);
+  res.send({ chef });
+});
+
 app.listen(5000, () => {
   console.log("server is running");
 });
